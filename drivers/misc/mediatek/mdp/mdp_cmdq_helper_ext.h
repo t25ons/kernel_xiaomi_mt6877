2d8f47596d86d7e5f833afe19425001490d38439
fukiame <fukiame@proton.me>
2025-07-08 15:31:28 +0700

    drivers: mtk: mdp: ((void)0) all tracing
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/mdp/mdp_cmdq_helper_ext.h
+++ drivers/misc/mediatek/mdp/mdp_cmdq_helper_ext.h
@@ -167,55 +167,14 @@ do {if (1) mmprofile_log_ex(args); } while (0);	\
 #endif
 
 /* CMDQ FTRACE */
-#define CMDQ_TRACE_FORCE_BEGIN(fmt, args...) do { \
-	preempt_disable(); \
-	event_trace_printk(cmdq_get_tracing_mark(), \
-		"B|%d|"fmt, current->tgid, ##args); \
-	preempt_enable();\
-} while (0)
-
-#define CMDQ_TRACE_FORCE_END() do { \
-	preempt_disable(); \
-	event_trace_printk(cmdq_get_tracing_mark(), "E\n"); \
-	preempt_enable(); \
-} while (0)
-
-#define CMDQ_TRACE_FORCE_COUNTER(fmt, count, args...) do { \
-	preempt_disable(); \
-	event_trace_printk(cmdq_get_tracing_mark(), \
-		"C|%d|"fmt"|%d\n", current->tgid, ##args, count); \
-	preempt_enable(); \
-} while (0)
-
-#define CMDQ_SYSTRACE_BEGIN(fmt, args...) do { \
-	if (cmdq_core_ftrace_enabled()) { \
-		CMDQ_TRACE_FORCE_BEGIN(fmt, ##args); \
-	} \
-} while (0)
-
-#define CMDQ_SYSTRACE_END() do { \
-	if (cmdq_core_ftrace_enabled()) { \
-		CMDQ_TRACE_FORCE_END(); \
-	} \
-} while (0)
-
-#define CMDQ_SYSTRACE2_BEGIN(fmt, args...) do { \
-	if (cmdq_core_ftrace2_enabled()) { \
-		CMDQ_TRACE_FORCE_BEGIN(fmt, ##args); \
-	} \
-} while (0)
-
-#define CMDQ_SYSTRACE2_END() do { \
-	if (cmdq_core_ftrace2_enabled()) { \
-		CMDQ_TRACE_FORCE_END(); \
-	} \
-} while (0)
-
-#define CMDQ_SYSTRACE2_COUNTER(fmt, count, args...) do { \
-	if (cmdq_core_ftrace2_enabled()) { \
-		CMDQ_TRACE_FORCE_COUNTER(fmt, count, ##args); \
-	} \
-} while (0)
+#define CMDQ_TRACE_FORCE_BEGIN(fmt, args...) ((void)0)
+#define CMDQ_TRACE_FORCE_END() ((void)0)
+#define CMDQ_TRACE_FORCE_COUNTER(fmt, count, args...) ((void)0)
+#define CMDQ_SYSTRACE_BEGIN(fmt, args...) ((void)0)
+#define CMDQ_SYSTRACE_END() ((void)0)
+#define CMDQ_SYSTRACE2_BEGIN(fmt, args...) ((void)0)
+#define CMDQ_SYSTRACE2_END() ((void)0)
+#define CMDQ_SYSTRACE2_COUNTER(fmt, count, args...) ((void)0)
 
 #define CMDQ_GET_TIME_IN_MS(start, end, duration)	\
 {	\
